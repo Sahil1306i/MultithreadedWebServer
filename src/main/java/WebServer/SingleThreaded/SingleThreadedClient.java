74c558ed3f40739658f237c28ac78a3f36f1a880
sahil <www.sgt1625@gmail.com>
2025-05-06 20:27:52 +0530

    SingleThreadedServer is complete and now can be tested by running the client and the server side by side in the terminal now will have to update the readme file for the same;

--- src/main/java/WebServer/SingleThreaded/SingleThreadedClient.java
+++ src/main/java/WebServer/SingleThreaded/SingleThreadedClient.java
@@ -1,4 +1,33 @@
 package WebServer.SingleThreaded;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.rmi.UnknownHostException;
+
 public class SingleThreadedClient {
+    public void run() throws UnknownHostException, IOException {
+        // here we are checking for the host exception and Io exception
+        int port = 8010;
+        InetAddress address = InetAddress.getByName("localhost");
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello from the client"); // this is sent on the socket from the client side
+        String line = fromSocket.readLine();
+        System.out.println("Response from the socket is:"+line);
+    }
+
+    public static void main(String[] args) {
+        try{
+            SingleThreadedClient client = new SingleThreadedClient();
+            client.run();
+        } catch(Exception ex){
+        ex.printStackTrace();
+
+        }
+    }
 }
--- src/main/java/WebServer/SingleThreaded/SingleThreadedServer.java
+++ src/main/java/WebServer/SingleThreaded/SingleThreadedServer.java
@@ -1,23 +1,40 @@
 package WebServer.SingleThreaded;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class SingleThreadedServer {
-    public void run() throws IOException, UnknownHostException {
+    public void run() throws IOException {
         int port = 8010;// define the port where the server listens to the client
         ServerSocket socket = new ServerSocket(port);
-        socket.setSoTimeout(10000);
+        socket.setSoTimeout(20000);
         while(true){
             System.out.println("Server is listning on port" + port);
-            Socket aacceptedConnection = socket.accept();
+            Socket acceptedConnection = socket.accept();
             // code will be stopping here until there is no connection from the client
+            System.out.println("Connection accepted"+ acceptedConnection.getRemoteSocketAddress());
+            PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
+            //printwriter needs an output stream and getoutput steam does that
+            // printwriter helps us to get the data from the output stream from the server it is a class
 
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+            toClient.println("Hello from the server");
+
+            break;
         }
     }
     public static void main(String[] args) {
+        SingleThreadedServer server = new SingleThreadedServer();
+        // the run method is not a static method so it is not in the memory so will have to make a instace for that
+        try{
+            server.run();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
 
     }
 }
